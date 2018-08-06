519dd936e1a548bb789565dbdf1b3273a0d8f523
Kirill <revenkov-k@mail.ru>
2018-08-06 15:35:27 +0300

    new version of signing
    change main library to forge (basis of keypair.js) for compability with server
    change keys generation algorithm to rsa
    output format of keys: pem pksc#1
    rename some procedures

--- digitalSignature.js
+++ digitalSignature.js
@@ -1,70 +1,88 @@
 /**
  * Digital Signature for Candy
- * 
- * required jsrsasign (jsrsasign-all-min.js)
+ * uses SHA256withRSA algorithm
+ * required forge.min.js
+ * https://github.com/digitalbazaar/forge
  */
 
 'use strict';
 
+
 function DigitalSignature(dataToSign) { //data in string format
 	
 	/**
-     * EC keys for sign
+     * RSA keys for sign
      */
 	this.keysPair = {};
+   
 	/**
-     * curve's name for generating keys
+     * Sign
      */
-	this.curve = 'secp256k1';
-	/**
-     * Sign in HEX format
-     */
-	this.signHex = '';
+	this.sign = '';
 	/**
      * Sign algorithm
      */
-	this.sigAlg = 'SHA256withECDSA';
+	this.sigAlg = 'SHA256withRSA';
 	/**
      * data format as presented in 'block data'
      */
 	this.signedData = {
-		data:'',
-		sign:'',
-		pubkey:''
+		data: dataToSign,     //incoming data
+		sign:'',              //sign in HEX format
+		pubkey:''             //Public key in pem PKCS#1
 	};
 	
 	/**
      * Generate pair of keys for signing
-     * @param {curve} name of the curve
+     * @param {len} length of the key
      */
-	this.generateKeys = (curve = this.curve) => {
-		let ec = new KJUR.crypto.ECDSA({"curve": curve});
-		let keypair = ec.generateKeyPairHex();
-		this.keysPair.private = keypair.ecprvhex;
-		//ExportPublicToPEM
-		this.keysPair.public = KEYUTIL.getPEM(KEYUTIL.getKey({xy: keypair.ecpubhex, curve: curve}));
-		return this.keysPair;
-	};
-	
+	this.generate = (len = 2048) => {
+		
+        let rsa = forge.pki.rsa;
+        let keypair = forge.rsa.generateKeyPair({len});
+        keypair = {
+            public: repairKey(fix(forge.pki.publicKeyToRSAPublicKeyPem(keypair.publicKey, 72))),
+            private: repairKey(fix(forge.pki.privateKeyToPem(keypair.privateKey, 72)))
+        };
+        console.log('Info: Keypair generated');
+        return keypair;
+    };
+    
+    
+    function fix (str) {
+      return str.replace(/\r/g, '') + '\n'
+    }
+    
+    /**
+     * Repair bad generated key
+     * @param key
+     * @return {*}
+     */
+    function repairKey(key) {
+        if(key[key.length - 1] !== "\n") {
+            key += "\n";
+        }
+        return key.replace(new RegExp("\n\n", 'g'),"\n");
+}
 	
 	/**
      * Signs data
 	 * @param {data} data for signing
 	 * @param {privateKey} private key
-	 * @param {curve} name of the curve
-	 * @param {sigAlg} name of the algorithm
      */
-	this.signData = (data = dataToSign, privateKey = this.keysPair.private, curve = this.curve, sigAlg = this.sigAlg) => {
+    
+    
+	this.signData = (data = dataToSign, key = this.keysPair.private) => {
 		if (!data) {
 			console.log('No data to sign');
 			return '';
 		};
-		var msg = data;
-		var sig = new KJUR.crypto.Signature({"alg": sigAlg});
-		sig.init({d: privateKey, curve: curve});
-		sig.updateString(msg);
-		this.signHex = sig.sign();
-		return this.signHex; 
+        let md= forge.md.sha256.create();
+        md.update(data,'utf8');
+        let privateKey = forge.pki.privateKeyFromPem(key); 
+        this.sign = privateKey.sign(md); 
+        console.log('Info: Data signed');
+		return {data: data, sign: forge.util.bytesToHex(this.sign)}; 
 	};
 	
 	
@@ -72,16 +90,17 @@ function DigitalSignature(dataToSign) { //data in string format
      * Signs data
 	 * @param {data} data for signing
 	 * @param {puBkey} private key
-	 * @param {sigAlg} name of the algorithm
-	 * @param {sigVal} signature value
      */
-	this.verify = (data, puBkey = this.keysPair.public, sigVal = this.signHex, sigAlg = this.sigAlg) => {
-		try {
-			var msg = data;
-			var sig = new KJUR.crypto.Signature({"alg": sigAlg});
-			sig.init(puBkey);
-			sig.updateString(data);
-			return sig.verify(sigVal);
+	this.verifyData = (data = this.signedData, sign = this.signedData.sign, key = this.keysPair.public) => {
+		if (typeof data === 'object'){
+            sign = data.sign;
+            data = data.data;
+        };
+        try {
+            let publicKey = forge.pki.publicKeyFromPem(key);
+            let md = forge.md.sha256.create();
+            md.update(data,'utf8');
+			return publicKey.verify(md.digest().bytes(), forge.util.hexToBytes(sign)); //verifying only in bytes format
 		} catch (e){
 			console.log(e);
             return false;
@@ -89,9 +108,12 @@ function DigitalSignature(dataToSign) { //data in string format
 	};
 	
 	if (dataToSign !== undefined){
-		this.signedData.data = dataToSign;
-		this.signedData.pubkey = this.generateKeys();
-		this.signedData.sign = this.signData();
+        this.keysPair = this.generate();
+        this.signedData.pubkey = this.keysPair.public;
+        this.signedData.sign = this.signData().sign;
+        if(this.verifyData() === false) {
+            console.log('Sign self-validation error! Invalid key or sign checking');
+        }
 	};
 	
 	return this;
